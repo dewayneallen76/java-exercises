16d12534c1df4b55eeaf8dcbf209022e3713f193
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-11 11:31:42 -0500

    refactored main class to have two outputs

--- java-exercises/src/HelloWorld.java
+++ java-exercises/src/HelloWorld.java
@@ -3,6 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Hello World!");
+        System.out.print("Welcome to Java");
     }
 }
