c82e22c3318fe431d8b7153871b4f0c4bb228363
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-11 11:53:52 -0500

    added integer variable and string variable and printed out the output

--- java-exercises/src/HelloWorld.java
+++ java-exercises/src/HelloWorld.java
@@ -3,7 +3,16 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("Welcome to Java");
+        System.out.println("Hello World!");
+        System.out.println("Welcome to Java");
+
+        int myFavoriteNumber = 4356;
+        String myString = "Whats up dum dum";
+
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
     }
+
+
+
 }
