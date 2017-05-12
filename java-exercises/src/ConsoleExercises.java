9e31020e972b95a53893e42c0bd4a13d06d3b649
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-12 09:45:23 -0500

    working on console exercises using the Scanner Class

--- java-exercises/src/ConsoleExercises.java
+++ java-exercises/src/ConsoleExercises.java
@@ -13,5 +13,13 @@ public class ConsoleExercises {
         userInput = sc.nextLine();
 
         System.out.println("You entered: ---> \"" + userInput + "\" <--");
+
+        int userInt;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        userInt = sc.nextLine();
+
+        System.out.format("You entered the number : " + userInt);
     }
 }
