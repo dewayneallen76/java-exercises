f09c1587c1fe6acc44ff52488c41b89f8b93dc12
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-13 12:13:42 -0500

    added new class ControlFlowExercies

---
+++ java-exercises/src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+/**
+ * Created by dewayneallen on 5/13/17.
+ */
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+        int integer = 5;
+
+        while(integer <= 15) {
+            System.out.println(integer++);
+        }
+
+    }
+}
