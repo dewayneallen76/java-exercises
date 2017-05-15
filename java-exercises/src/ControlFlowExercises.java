c45374798eb8397d3bcafdc88b8689916643e76d
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-14 21:04:22 -0500

    refactored fizzbuzz

--- java-exercises/src/ControlFlowExercises.java
+++ java-exercises/src/ControlFlowExercises.java
@@ -6,20 +6,17 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         int i;
 
-        for(i = 0; i <= 100; i++) {
-            if((i % 3) == 0) {
-                System.out.println("Fizz");
-            }
-            if((i % 5) == 0) {
-                System.out.println("Buzz");
-            }
-            if((i % 3) == 0 && i % 5 == 0) {
+        for (i = 1; i <= 100; i++) {
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
                 System.out.println("FizzBuzz");
+            } else if ((i % 3) == 0) {
+                System.out.println("Fizz");
+            } else if ((i % 5) == 0) {
+                System.out.println("Buzz");
             } else {
-                System.out.println(String.valueOf(i));
+                System.out.println(i);
             }
-
         }
-
     }
 }
+
