8811717739eabe8fb9f744768c2996915388483a
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-16 11:31:16 -0500

    added do while loop counting from 2 to 100

--- java-exercises/src/ControlFlowExercises.java
+++ java-exercises/src/ControlFlowExercises.java
@@ -4,7 +4,10 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-
+        int i = 0;
+        do {
+            System.out.println(i = i + 2);
+        } while (i < 100);
     }
 }
 
