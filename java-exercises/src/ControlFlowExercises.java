76f2d3798bfaaf06a7613f883d221ff8841353d8
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-16 15:17:44 -0500

    cleared the main method in the control flow exercises

--- java-exercises/src/ControlFlowExercises.java
+++ java-exercises/src/ControlFlowExercises.java
@@ -4,10 +4,7 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 100;
-        do {
-            System.out.println(i = i - 5);
-        } while (i > -10);
+
     }
 }
 
