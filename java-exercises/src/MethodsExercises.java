07fadd0dbffe68b846f1f4d33604ed29f18b2bb0
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-17 16:14:42 -0500

    added output to use with addition submtraction and multiplication methods that were written

--- java-exercises/src/MethodsExercises.java
+++ java-exercises/src/MethodsExercises.java
@@ -10,15 +10,17 @@ public class MethodsExercises {
         return (a + b);
     }
 
-    public int subtractNumbers(int a, int b) {
+    public static int subtractNumbers(int a, int b) {
         return (a - b);
     }
 
-    public int multiplyNumbers(int a, int b) {
+    public static int multiplyNumbers(int a, int b) {
         return (a * b);
     }
 
     public static void main(String[] args) {
-        System.out.println("The sum of the numbers is " + addNumbers(5,5));
+        System.out.println("Addition 5 + 5 using addNumbers method = " + addNumbers(5,5));
+        System.out.println("Subtraction 10 - 5 using subtractNumbers method = " + subtractNumbers(10, 5));
+        System.out.println("Multiply 4 * 4 using the mutliplyNumber method = " + multiplyNumbers(4, 4));
     }
 }
