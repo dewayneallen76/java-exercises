11199814288dc446b2cf311a02481c351e4c7dd7
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-18 14:51:09 -0500

    added condition to divideNumbers method to display error message if trying to divide by zero

--- java-exercises/src/MethodsExercises.java
+++ java-exercises/src/MethodsExercises.java
@@ -19,13 +19,16 @@ public class MethodsExercises {
     }
 
     public static int divideNumbers(int a, int b) {
-        return (a / b);
+        if (a == 0 || b == 0) {
+            System.out.println("You cannot divide by zero");
+        }
+        return (a/b);
     }
 
     public static void main(String[] args) {
         System.out.println("Addition 5 + 5 using addNumbers method = " + addNumbers(5,5));
         System.out.println("Subtraction 10 - 5 using subtractNumbers method = " + subtractNumbers(10, 5));
         System.out.println("Multiply 4 * 4 using the mutliplyNumber method = " + multiplyNumbers(4, 4));
-        System.out.println("Divide 10 by 2 using the divideNumber method = " + divideNumbers(10, 2));
+        System.out.println("Divide 10 by 5 using the divideNumber method = " + divideNumbers(10, 5));
     }
 }
