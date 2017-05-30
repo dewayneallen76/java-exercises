470cdf5daac970a0774c6add86e89b7133235ac1
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 12:29:10 -0500

    added main method to list the options to the user

--- java-exercises/src/contacts/ContactsManagerApplication.java
+++ java-exercises/src/contacts/ContactsManagerApplication.java
@@ -55,5 +55,47 @@ public class ContactsManagerApplication {
         System.out.println("Contact removed!");
     }
 
-    public
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------------");
+        System.out.println(" > Welcome to the contacts manager!");
+        System.out.println("-------------------------------------------------------------");
+
+        while (true) {
+            System.out.println("What would you like to do?");
+
+            System.out.println("0 - Exit");
+            System.out.println("1 - View Contacts");
+            System.out.println("2 - Add a new contact");
+            System.out.println("3 - Search by name");
+            System.out.println("4 - Search by number");
+            System.out.println("5 - Remove an existing contact");
+
+            int choice = Input.getInt("Please make a selection: ");
+
+            switch(choice) {
+                case 0:
+                    System.out.println("Have a great day!");
+                    return;
+                case 1:
+                    showContacts();
+                    break;
+                case 2:
+                    addContact();
+                    break;
+                case 3:
+                    searchByName();
+                    break;
+                case 4:
+                    searchByNumber();
+                    break;
+                case 5:
+                    removeContact();
+                    break;
+                default:
+                    System.out.println("Unknown selection, " + choice + ".");
+                    return;
+            }
+        }
+
+    }
 }
