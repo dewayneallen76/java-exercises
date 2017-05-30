517c3d383371da719778a950bf4c17c674a7aa36
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 11:34:29 -0500

    added methods

--- java-exercises/src/contacts/ContactsManagerApplication.java
+++ java-exercises/src/contacts/ContactsManagerApplication.java
@@ -1,5 +1,7 @@
 package contacts;
 
+import java.io.IOException;
+
 /**
  * Created by dewayneallen on 5/30/17.
  */
@@ -19,4 +21,39 @@ public class ContactsManagerApplication {
         System.out.println();
     }
 
+    public static void addContact() {
+        Contact newContact = new Contact(
+                Input.getString("name: "),
+                Input.getString("number: ")
+        );
+
+        try{
+            contactsDao.add(newContact);
+            System.out.println("Contact added!");
+        } catch (IOException e) {
+            System.out.println("Error adding new contact!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void searchByName() {
+
+    }
+
+    public static void searchByNumber() {
+
+    }
+
+    public static void removeContact() {
+        String name = Input.getString("Enter the name of the contact to remove: ");
+        Contact toRemove = contactsDao.findByName(name);
+        if(toRemove == null) {
+            System.out.println("Sorry, no contact by that name was found.");
+            return;
+        }
+        contactsDao.delete(toRemove);
+        System.out.println("Contact removed!");
+    }
+
+    public
 }
