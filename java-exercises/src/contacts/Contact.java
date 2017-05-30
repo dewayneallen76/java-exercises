dcb4a20902851294edfdfa585ec9b8134767c4c0
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 10:18:50 -0500

    updated Contact class

--- java-exercises/src/contacts/Contact.java
+++ java-exercises/src/contacts/Contact.java
@@ -4,4 +4,13 @@ package contacts;
  * Created by dewayneallen on 5/30/17.
  */
 public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+
 }
