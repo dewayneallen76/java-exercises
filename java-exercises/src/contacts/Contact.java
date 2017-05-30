341fa0ec2f2cf91b6cf2cb06c20129be067d565d
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 10:21:53 -0500

    added getters and setters to the contact class

--- java-exercises/src/contacts/Contact.java
+++ java-exercises/src/contacts/Contact.java
@@ -12,5 +12,20 @@ public class Contact {
         this.number = number;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
 }
