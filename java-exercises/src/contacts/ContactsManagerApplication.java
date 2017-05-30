6844f4d40437e8d1083533478ecca23960c1430f
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 10:45:49 -0500

    added showContacts method

--- java-exercises/src/contacts/ContactsManagerApplication.java
+++ java-exercises/src/contacts/ContactsManagerApplication.java
@@ -6,5 +6,17 @@ package contacts;
 public class ContactsManagerApplication {
     private static Contacts contactsDao = new TextFileContacts();
 
+    public static void showContacts() {
+        System.out.println();
+        System.out.println(" Name | Number ");
+        for(Contact contact : contactsDao.findAll()) {
+            System.out.printf(
+                    "%s | %s\n",
+                    contact.getName(),
+                    contact.getNumber()
+            );
+        }
+        System.out.println();
+    }
 
 }
