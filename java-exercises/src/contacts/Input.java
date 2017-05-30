e25640b00dd3912afd855a8920f3c90654773485
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 12:18:21 -0500

    added getInt and getString methods to the Input class

--- java-exercises/src/contacts/Input.java
+++ java-exercises/src/contacts/Input.java
@@ -1,7 +1,34 @@
 package contacts;
 
+import java.util.Scanner;
+
 /**
  * Created by dewayneallen on 5/30/17.
  */
 public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int getInt() {
+        return getInt("Please enter an integer: ");
+    }
+
+    public static int getInt(String prompt) {
+        System.out.print(prompt);
+        String userInput = scanner.nextLine();
+        try {
+            return Integer.valueOf(userInput);
+        } catch (NumberFormatException e) {
+            System.out.println("Sorry, '" + userInput + "' is not a valid integer.");
+            return  getInt(prompt);
+        }
+    }
+
+    public static String getString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public static String getString() {
+        return getString("Please enter a string: ");
+    }
 }
