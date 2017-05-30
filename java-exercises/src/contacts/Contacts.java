cff97133387646b4e6d2bc5f7d62fb0885bdf866
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-30 10:39:55 -0500

    added interface

--- java-exercises/src/contacts/Contacts.java
+++ java-exercises/src/contacts/Contacts.java
@@ -1,7 +1,15 @@
 package contacts;
 
+import java.util.List;
+import java.io.IOException;
+
 /**
  * Created by dewayneallen on 5/30/17.
  */
 public interface Contacts {
+    List<Contact> findAll;
+    Contact findByName(String name);
+    Contact findByNumber(String number);
+    void add(Contact Contact) throws IOException;
+    void delete(Contact contact);
 }
