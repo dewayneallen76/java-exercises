95fdd554d2d0a88f6f467d592da91a333fa730a3
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-31 10:08:01 -0500

    did not close method findAll properly

--- java-exercises/src/contacts/Contacts.java
+++ java-exercises/src/contacts/Contacts.java
@@ -7,7 +7,7 @@ import java.io.IOException;
  * Created by dewayneallen on 5/30/17.
  */
 public interface Contacts {
-    List<Contact> findAll;
+    List<Contact> findAll();
     Contact findByName(String name);
     Contact findByNumber(String number);
     void add(Contact Contact) throws IOException;
