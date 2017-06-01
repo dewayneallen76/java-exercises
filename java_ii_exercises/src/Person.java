e439e1b436e8716a03687c29c307033566d1dbb9
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-01 11:14:37 -0500

    added person class

---
+++ java_ii_exercises/src/Person.java
@@ -0,0 +1,18 @@
+/**
+ * Created by dewayneallen on 6/1/17.
+ */
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        this.name = name;
+    };
+
+    public void setName(String name) {
+
+    }
+
+    public void sayHello() {
+    }
+}
